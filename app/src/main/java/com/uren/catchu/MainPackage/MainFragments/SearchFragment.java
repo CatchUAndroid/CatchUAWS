9f1142061fed2415110ba0b245057fb7064ba5d3
ASUS <topaloglun@itu.edu.tr>
2018-08-06 13:39:19 +0300

    Search barında getFriends demosu eklendi

--- app/src/main/java/com/uren/catchu/MainPackage/MainFragments/SearchFragment.java
+++ app/src/main/java/com/uren/catchu/MainPackage/MainFragments/SearchFragment.java
@@ -1,15 +1,22 @@
 package com.uren.catchu.MainPackage.MainFragments;
 
+import android.annotation.SuppressLint;
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.amazonaws.mobileconnectors.apigateway.ApiClientFactory;
 import com.uren.catchu.MainPackage.NextActivity;
 import com.uren.catchu.R;
 
 import butterknife.ButterKnife;
-
+import catchu.CatchUMobileAPIClient;
+import catchu.model.SearchResult;
 
 
 public class SearchFragment extends BaseFragment{
@@ -33,6 +40,39 @@ public class SearchFragment extends BaseFragment{
 
 
         return view;
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        getFriends();
+
+    }
+
+    @SuppressLint("StaticFieldLeak")
+    private void getFriends() {
+
+        new AsyncTask<Void, Void, Void>(){
+
+            ApiClientFactory factory = new ApiClientFactory();
+            CatchUMobileAPIClient client = factory.build(CatchUMobileAPIClient.class);
+
+
+            @Override
+            protected Void doInBackground(Void... voids) {
+
+
+                SearchResult searchResult = client.searchGet("us-east-1:4af861e4-1cb6-4218-87e7-523c84bbfa96", "e");
+                Log.i("resultArray ", searchResult.getResultArray().get(0).toString());
+
+                return null;
+
+
+            }
+        }.execute();
+
+
     }
 
 
