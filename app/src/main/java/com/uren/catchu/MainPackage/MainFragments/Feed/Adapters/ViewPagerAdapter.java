4ee1cc61711cabcc6764b36b864d3c46732320b5
ASUS <topaloglun@itu.edu.tr>
2018-09-13 16:40:23 +0300

    development go on

--- app/src/main/java/com/uren/catchu/MainPackage/MainFragments/Feed/Adapters/ViewPagerAdapter.java
+++ app/src/main/java/com/uren/catchu/MainPackage/MainFragments/Feed/Adapters/ViewPagerAdapter.java
@@ -2,33 +2,22 @@ package com.uren.catchu.MainPackage.MainFragments.Feed.Adapters;
 
 
 import android.content.Context;
-import android.media.Image;
 import android.media.MediaPlayer;
 import android.net.Uri;
 import android.support.v4.view.PagerAdapter;
-import android.support.v4.view.ViewPager;
-import android.support.v7.widget.CardView;
-import android.support.v7.widget.RecyclerView;
-import android.util.DisplayMetrics;
-import android.util.Log;
 import android.view.LayoutInflater;
-import android.view.MotionEvent;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
-import android.widget.TextView;
 import android.widget.VideoView;
 
 import com.bumptech.glide.Glide;
 import com.bumptech.glide.request.RequestOptions;
-import com.uren.catchu.MainPackage.MainFragments.Feed.JavaClasses.ModelObject;
 import com.uren.catchu.R;
 
 import java.util.List;
-import java.util.Random;
 
 public class ViewPagerAdapter extends PagerAdapter {
-
 
     private Context mContext;
     private List<String> imageList;
@@ -37,7 +26,6 @@ public class ViewPagerAdapter extends PagerAdapter {
     private int videoCounter;
     MediaPlayer mediaPlayer;
     ImageView playVideoImgv;
-    int mediaLen = 0;
 
     public ViewPagerAdapter(Context context, List<String> imageList, List<String> videoList) {
         this.mContext = context;
@@ -49,9 +37,6 @@ public class ViewPagerAdapter extends PagerAdapter {
 
     @Override
     public Object instantiateItem(ViewGroup collection, int position) {
-        //ModelObject modelObject = ModelObject.values()[position];
-        //LayoutInflater inflater = LayoutInflater.from(mContext);
-        //ViewGroup layout = (ViewGroup) inflater.inflate(modelObject.getLayoutResId(), collection, false);
 
         View itemView;
 
