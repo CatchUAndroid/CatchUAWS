55eeab3b1ca8c1ceb912d36c9b40da2656dfaf17
ugurgog <32577192+ugurgog@users.noreply.github.com>
2018-09-24 11:29:48 +0300

    video record go on

--- app/src/main/java/com/uren/catchu/SharePackage/Adapters/ShareItemsDisplayAdapter.java
+++ app/src/main/java/com/uren/catchu/SharePackage/Adapters/ShareItemsDisplayAdapter.java
@@ -4,7 +4,11 @@ import android.content.Context;
 import android.graphics.Bitmap;
 import android.media.MediaPlayer;
 import android.net.Uri;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
 import android.support.v4.view.PagerAdapter;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -12,141 +16,58 @@ import android.widget.ImageView;
 
 import com.bumptech.glide.Glide;
 import com.bumptech.glide.request.RequestOptions;
+import com.uren.catchu.MainPackage.MainFragments.Feed.JavaClasses.MediaSerializable;
+import com.uren.catchu.MainPackage.MainFragments.Feed.SubFragments.ImageFragment;
+import com.uren.catchu.MainPackage.MainFragments.Feed.SubFragments.VideoFragment;
 import com.uren.catchu.R;
 
 import java.util.List;
 
-public class ShareItemsDisplayAdapter extends PagerAdapter{
+import catchu.model.Media;
 
-    private Context mContext;
-    private List<Uri> imageList;
-    private List<Uri> videoList;
-    private List<Bitmap> textBitmapList;
-    private int imageCounter;
-    private int videoCounter;
-    private int textCounter;
-    MediaPlayer mediaPlayer;
-    ImageView playVideoImgv;
+import static com.uren.catchu.Constants.StringConstants.IMAGE_TYPE;
+import static com.uren.catchu.Constants.StringConstants.VIDEO_TYPE;
 
-    public ShareItemsDisplayAdapter(Context context, List<Bitmap> textBitmapList, List<Uri> imageList, List<Uri> videoList) {
-        this.mContext = context;
-        this.imageList = imageList;
-        this.videoList = videoList;
-        this.textBitmapList = textBitmapList;
-        this.imageCounter = 0;
-        this.videoCounter = 0;
-        this.textCounter = 0;
+public class ShareItemsDisplayAdapter extends FragmentPagerAdapter {
+
+    private List<Media> mediaList;
+    private int bindedItems;
+
+    public ShareItemsDisplayAdapter(FragmentManager fragmentManager, List<Media> mediaList) {
+        super(fragmentManager);
+
+        this.mediaList= mediaList;
+        this.bindedItems=0;
     }
 
-    @Override
-    public Object instantiateItem(ViewGroup collection, int position) {
-
-        View itemView;
-
-        if(textCounter < textBitmapList.size()){
-            itemView = LayoutInflater.from(mContext)
-                    .inflate(R.layout.viewpager_image, collection, false);
-            ImageView imgFeedItem = (ImageView) itemView.findViewById(R.id.imgFeedItem);
-            collection.addView(itemView);
-
-            Glide.with(mContext)
-                    .load(textBitmapList.get(textCounter))
-                    .apply(RequestOptions.circleCropTransform())
-                    .into(imgFeedItem);
-
-            textCounter++;
-        }
-
-        if (imageCounter < imageList.size()) {
-            itemView = LayoutInflater.from(mContext)
-                    .inflate(R.layout.viewpager_image, collection, false);
-            ImageView imgFeedItem = (ImageView) itemView.findViewById(R.id.imgFeedItem);
-            collection.addView(itemView);
-
-            Glide.with(mContext)
-                    .load(imageList.get(imageCounter))
-                    .apply(RequestOptions.centerInsideTransform())
-                    .into(imgFeedItem);
-
-            imageCounter++;
-        }
-
-        if (videoCounter < videoList.size()) {
-            itemView = LayoutInflater.from(mContext)
-                    .inflate(R.layout.viewpager_video, collection, false);
-            //VideoView videoFeedItem = (VideoView) itemView.findViewById(R.id.videoFeedItem); ugurfix
-            collection.addView(itemView);
-
-            playVideoImgv = itemView.findViewById(R.id.playVideoImgv);
-            /*videoFeedItem.setVideoURI(videoList.get(videoCounter));
-            videoFeedItem.requestFocus();
-
-            videoFeedItem.setOnPreparedListener(new MediaPlayer.OnPreparedListener() {
-                public void onPrepared(MediaPlayer mp) {
-                    mediaPlayer = mp;
-                    mediaPlayer.start();
-                    playVideoImgv.setVisibility(View.GONE);
-                }
-            });
-
-            videoFeedItem.setOnClickListener(new View.OnClickListener() {
-                @Override
-                public void onClick(View v) {
-                    if (playVideoImgv.getVisibility() == View.GONE) {
-                        playVideoImgv.setVisibility(View.VISIBLE);
-                        mediaPlayer.pause();
-                    } else {
-                        playVideoImgv.setVisibility(View.GONE);
-                        mediaPlayer.start();
-                    }
-                }
-            });*/ // ugurfix
-
-            videoCounter++;
-
-        } else if (imageCounter < imageList.size()) {
-            //sonra imagelar bitene kadar eklenir
-
-            itemView = LayoutInflater.from(mContext)
-                    .inflate(R.layout.viewpager_image, collection, false);
-            ImageView imgFeedItem = (ImageView) itemView.findViewById(R.id.imgFeedItem);
-            collection.addView(itemView);
-
-            Glide.with(mContext)
-                    .load(imageList.get(imageCounter))
-                    .apply(RequestOptions.centerInsideTransform())
-                    .into(imgFeedItem);
-
-            imageCounter++;
-
-        } else {
-            //do nothing
-            itemView = LayoutInflater.from(mContext)
-                    .inflate(R.layout.view_red, collection, false);
-        }
-
-        return itemView;
-    }
-
-    @Override
-    public void destroyItem(ViewGroup collection, int position, Object view) {
-        // NT: kaydırma esnasında view'lar kayboldugu icin kapattım
-        //collection.removeView((View) view);
-    }
-
+    // Returns total number of pages
     @Override
     public int getCount() {
-        return imageList.size() + videoList.size();
+        return mediaList.size();
     }
 
+    // Returns the fragment to display for that page
     @Override
-    public boolean isViewFromObject(View view, Object object) {
-        return view == object;
+    public Fragment getItem(int position) {
+
+        MediaSerializable mediaSerializable = new MediaSerializable(mediaList.get(position));
+
+        if(mediaList.get(position).getType().equals(VIDEO_TYPE)){
+            return VideoFragment.newInstance(mediaSerializable);
+        }else if(mediaList.get(position).getType().equals(IMAGE_TYPE)){
+            return ImageFragment.newInstance(mediaSerializable);
+        }else{
+            Log.e("mediaTypeError ", "unknown_media_type");
+        }
+
+        return null;
     }
 
+    // Returns the page title for the top indicator
     @Override
     public CharSequence getPageTitle(int position) {
-        String pageTitle = "Title";
-        return pageTitle;
+        return "Page " + position;
     }
+
 }
+
