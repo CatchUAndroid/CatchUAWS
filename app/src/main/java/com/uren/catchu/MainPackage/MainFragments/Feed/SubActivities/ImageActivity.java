656fbd9287fe22f555ea3846a8c35d05af2e84d4
ASUS <topaloglun@itu.edu.tr>
2018-11-09 13:15:37 +0300

    viewpager image onLoading eklendi/imageAct, videoAct

--- app/src/main/java/com/uren/catchu/MainPackage/MainFragments/Feed/SubActivities/ImageActivity.java
+++ app/src/main/java/com/uren/catchu/MainPackage/MainFragments/Feed/SubActivities/ImageActivity.java
@@ -1,12 +1,20 @@
 package com.uren.catchu.MainPackage.MainFragments.Feed.SubActivities;
 
+import android.graphics.drawable.Drawable;
+import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.ProgressBar;
 
 import com.bumptech.glide.Glide;
+import com.bumptech.glide.load.DataSource;
+import com.bumptech.glide.load.engine.GlideException;
+import com.bumptech.glide.request.RequestListener;
 import com.bumptech.glide.request.RequestOptions;
+import com.bumptech.glide.request.target.Target;
 import com.uren.catchu.MainPackage.MainFragments.Feed.JavaClasses.PostItem;
 import com.uren.catchu.R;
 
@@ -18,6 +26,12 @@ public class ImageActivity extends AppCompatActivity {
 
     @BindView(R.id.imgFeedItem)
     ImageView imgFeedItem;
+
+    @BindView(R.id.progressLoading)
+    ProgressBar progressLoading;
+
+    @BindView(R.id.llProgress)
+    LinearLayout llProgress;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -36,6 +50,19 @@ public class ImageActivity extends AppCompatActivity {
         Glide.with(this)
                 .load(media.getUrl())
                 .apply(RequestOptions.centerInsideTransform())
+                .listener(new RequestListener<Drawable>() {
+                    @Override
+                    public boolean onLoadFailed(@Nullable GlideException e, Object model, Target<Drawable> target, boolean isFirstResource) {
+                        return false;
+                    }
+
+                    @Override
+                    public boolean onResourceReady(Drawable resource, Object model, Target<Drawable> target, DataSource dataSource, boolean isFirstResource) {
+                        progressLoading.setVisibility(View.GONE);
+                        llProgress.setVisibility(View.GONE);
+                        return false;
+                    }
+                })
                 .into(imgFeedItem);
 
     }
--- app/src/main/java/com/uren/catchu/MainPackage/MainFragments/Feed/SubActivities/VideoActivity.java
+++ app/src/main/java/com/uren/catchu/MainPackage/MainFragments/Feed/SubActivities/VideoActivity.java
@@ -1,25 +1,42 @@
 package com.uren.catchu.MainPackage.MainFragments.Feed.SubActivities;
 
+import android.graphics.drawable.Drawable;
+import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.LinearLayout;
+import android.widget.ProgressBar;
 
 import com.bumptech.glide.Glide;
+import com.bumptech.glide.load.DataSource;
+import com.bumptech.glide.load.engine.GlideException;
+import com.bumptech.glide.request.RequestListener;
+import com.bumptech.glide.request.target.Target;
 import com.uren.catchu.MainPackage.MainFragments.Feed.JavaClasses.MyVideoModel;
 import com.uren.catchu.MainPackage.MainFragments.Feed.JavaClasses.PostItem;
 import com.uren.catchu.R;
 import com.uren.catchu._Libraries.VideoPlay.VideoPlay;
 
+import butterknife.BindView;
+import butterknife.ButterKnife;
 import catchu.model.Media;
 
 public class VideoActivity extends AppCompatActivity {
 
     private VideoPlay videoPlay;
 
+    @BindView(R.id.progressLoading)
+    ProgressBar progressLoading;
+
+    @BindView(R.id.llProgress)
+    LinearLayout llProgress;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_video);
+        ButterKnife.bind(this);
 
         setVideo();
 
@@ -45,6 +62,19 @@ public class VideoActivity extends AppCompatActivity {
 
         Glide.with(this)
                 .load(media.getThumbnail())
+                .listener(new RequestListener<Drawable>() {
+                    @Override
+                    public boolean onLoadFailed(@Nullable GlideException e, Object model, Target<Drawable> target, boolean isFirstResource) {
+                        return false;
+                    }
+
+                    @Override
+                    public boolean onResourceReady(Drawable resource, Object model, Target<Drawable> target, DataSource dataSource, boolean isFirstResource) {
+                        progressLoading.setVisibility(View.GONE);
+                        llProgress.setVisibility(View.GONE);
+                        return false;
+                    }
+                })
                 .into(videoPlay.getImageView());
 
 
