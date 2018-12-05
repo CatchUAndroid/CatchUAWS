fd37a0116bf65fc1a84a3d585f2ee95a5c830c83
ugurgog <32577192+ugurgog@users.noreply.github.com>
2018-12-05 15:31:22 +0300

    Ek duzenlemeler devam ediyor.

--- app/src/main/java/com/uren/catchu/GeneralUtils/CommonUtils.java
+++ app/src/main/java/com/uren/catchu/GeneralUtils/CommonUtils.java
@@ -70,6 +70,7 @@ public class CommonUtils {
     public static final void showCustomToast(Context context, String message){
 
         if(context == null) return;
+        if(message == null || message.isEmpty()) return;
         LayoutInflater inflater = (LayoutInflater) context.getSystemService( Context.LAYOUT_INFLATER_SERVICE );
         View view = inflater.inflate( R.layout.layout_custom_toast, null );
         View layout = (LinearLayout) view.findViewById( R.id.custom_toast_container );
