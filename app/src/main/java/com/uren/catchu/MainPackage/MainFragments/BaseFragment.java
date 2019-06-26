5d8e54a0cecee8c9227b6445cb3dcc863c5f7e46
ugurgog <32577192+ugurgog@users.noreply.github.com>
2019-06-27 01:03:41 +0300

    devammmmm

--- app/src/main/java/com/uren/catchu/MainPackage/MainFragments/BaseFragment.java
+++ app/src/main/java/com/uren/catchu/MainPackage/MainFragments/BaseFragment.java
@@ -2,10 +2,9 @@ package com.uren.catchu.MainPackage.MainFragments;
 
 import android.content.Context;
 import android.os.Bundle;
-import android.support.annotation.Nullable;
-import android.support.v4.app.Fragment;
 
-import com.uren.catchu.FragmentControllers.FragNavTransactionOptions;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
 
 
 public class BaseFragment extends Fragment {
